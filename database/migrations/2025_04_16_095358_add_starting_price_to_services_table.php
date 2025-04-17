e4dba513c61bcddd18009f83b7fbca448dc3161b
PM <PM@MacBook-Pro-de-Ludovic-2.local>
2025-04-17 13:37:05 +0000

    migration

--- database/migrations/2025_04_16_095358_add_starting_price_to_services_table.php
+++ database/migrations/2025_04_16_095358_add_starting_price_to_services_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('services', function (Blueprint $table) {
-            //
+            $table->decimal('starting_price', 10, 2)->nullable()->after('img_path');
         });
     }
 
@@ -22,7 +22,7 @@ return new class extends Migration
     public function down(): void
     {
         Schema::table('services', function (Blueprint $table) {
-            //
+            $table->dropColumn('starting_price');
         });
     }
 };
